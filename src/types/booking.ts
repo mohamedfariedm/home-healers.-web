53b4b31f0dd2df61cc7c1bc4858f846c09bed110
mohamedfariedm <mohamed.faried23267@gmail.com>
2025-10-21 21:43:17 +0300

    updates

--- src/types/booking.ts
+++ src/types/booking.ts
@@ -93,7 +93,7 @@ export interface Package {
   description: string
   price: string
   discount: string
-  image: any
+  image?: any
   type: string
   sessions_count?: number // Added to align with offers
 }
