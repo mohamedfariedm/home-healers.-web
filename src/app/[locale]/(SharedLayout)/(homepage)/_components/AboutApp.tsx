22c58df4f4762e5acb7dce4323ef003a291b12c9
mohamedfariedm <mohamed.faried23267@gmail.com>
2026-01-10 13:35:38 +0200

    handel updates button services

--- src/app/[locale]/(SharedLayout)/(homepage)/_components/AboutApp.tsx
+++ src/app/[locale]/(SharedLayout)/(homepage)/_components/AboutApp.tsx
@@ -122,19 +122,19 @@ function AboutApp({
         </div>
 
         {/* CTA Button */}
-        <motion.div
+        {/* <motion.div
           className="mt-6"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.3 }}
         >
           <Link
-            href={`${locale === "ar" ? "" : "/en"}/services`}
+            href={`${locale === "ar" ? "" : "/en"}/our-services`}
             className="flex items-center gap-2 px-4 py-2 bg-[#143087] text-white rounded-xl text-lg font-medium border border-[#143087]"
           >
             {locale === "ar" ? "جميع الخدمات" : "All Services"}
             <ArrowLeft className="w-6 h-6 text-white" />
           </Link>
-        </motion.div>
+        </motion.div> */}
       </motion.div>
 
       {/* Two Column Layout */}
